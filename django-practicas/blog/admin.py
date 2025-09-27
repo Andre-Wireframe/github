9ab55bf27e8a56d3b43c33c360cb6a803523fd4e
andre_wireframe <lima090403hsrzrna8@soycecytem.mx>
2025-09-27 11:50:52 -0600

    cree los models

--- django-practicas/blog/admin.py
+++ django-practicas/blog/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from .models import Post, Comment
 
 # Register your models here.
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'created_at', 'autor')
+    search_fields = ('title', 'autor')
+    list_filter = ('created_at', 'autor')
+
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('author', 'post', 'created_at')
+    search_fields = ('author',)
+    list_filter = ('created_at',)
+
+admin.site.register(Post, PostAdmin)
+admin.site.register(Comment, CommentAdmin)
--- django-practicas/blog/models.py
+++ django-practicas/blog/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
 # Create your models here.
+
+class Post(models.Model):
+    ID = models.AutoField(primary_key=True)
+    title = models.CharField(max_length=200)
+    autor = models.CharField(max_length=100)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
+    
+class Comment(models.Model):
+    ID = models.AutoField(primary_key=True)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    author = models.CharField(max_length=100)
+    text = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.author
