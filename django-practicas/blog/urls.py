00b211d6ac2ab2e09049a33b78ca5bba4f226aeb
andre_wireframe <lima090403hsrzrna8@soycecytem.mx>
2025-09-29 20:11:16 -0600

    Version alfa 1.0 lista

--- django-practicas/blog/urls.py
+++ django-practicas/blog/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.hola, name='hola'),
+    path('AddPost/', views.AddPost.as_view(), name='AddPost'),
+    path('AddComment/', views.AddComment.as_view(), name='AddComment'),
 ]
--- django-practicas/blog/views.py
+++ django-practicas/blog/views.py
@@ -1,6 +1,56 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.views.generic import View
+from .forms import PostForm, CommentForm
 
 # Create your views here.
 
 def hola(request):
     return render(request, 'hola.html', {})
+
+class AddPost(View):
+    def get(self, request, *args, **kwargs):
+        form = PostForm()
+        context = {
+            'form': form
+        }
+        return render(request, 'AddPost.html', context)
+    
+    def post(self, request, *args, **kwargs):
+
+        form = PostForm(request.POST)
+
+        if form.is_valid():
+            form.save()
+            context = {
+                'message': 'Post creado exitosamente'
+            }
+            return redirect('hola')
+        
+        context = {
+            'form': form
+        }
+        return render(request, 'AddPost.html', context)
+
+class AddComment(View):
+    def get(self, request, *args, **kwargs):
+        form = CommentForm()
+        context = {
+            'form': form
+        }
+        return render(request, 'AddComment.html', context)
+    
+    def post(self, request, *args, **kwargs):
+
+        form = CommentForm(request.POST)
+
+        if form.is_valid():
+            form.save()
+            context = {
+                'message': 'Comentario creado exitosamente'
+            }
+            return redirect('hola')
+        
+        context = {
+            'form': form
+        }
+        return render(request, 'AddComment.html', context)
