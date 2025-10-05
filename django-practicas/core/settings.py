9552845d1ab330916d503e720512056f1efa0ced
Andre-Wireframe <lima090403hsrzrna8@soycecytem.mx>
2025-10-04 19:34:18 -0600

    DEBUG false

--- django-practicas/core/settings.py
+++ django-practicas/core/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-^y$v+!f%e0l^zxlcln)0sh8%05zvrr^z!oeb96tdfp6j&e@7_*'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
